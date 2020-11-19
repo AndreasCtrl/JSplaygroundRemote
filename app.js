3939e866738a77dbe61e7368a08d995c29a480f1
AndreasCtrl <andrewkout@hotmail.com>
2020-11-19 20:33:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('button');
+btn.addEventListener('click', onOf);   
+function onOf() {
+    if(btn.innerText == 'On') {
+        btn.innerText = 'Off'
+    } else {
+        btn.innerText = 'On'
+    }
+}
+
+const h1 = document.querySelector('h1');
+h1.addEventListener('mouseover', () => h1.style.color='red');
+h1.addEventListener('mouseout', () => h1.style.color='black');
+
+
+const pic = document.querySelector('img');
+pic.addEventListener('mouseenter', () => pic.setAttribute('src', 'https://picsum.photos/200/300'));
